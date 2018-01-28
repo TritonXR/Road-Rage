d95825147e215b21e391a70e0ff48d2a8cb7b0e0
tiffanyc1107 <p938196@gmail.com>
2018-01-28 15:48:27 -0800

    add tool tips and point light

---
+++ Road-Rage-Master/Assets/ToolTipsMotion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolTipsMotion : MonoBehaviour {
+public GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (RemoveAfterSeconds (5, panel));
+	}
+	IEnumerator RemoveAfterSeconds(int seconds, GameObject obj){
+			yield return new WaitForSeconds(5);
+			obj.SetActive(false);
+		}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
