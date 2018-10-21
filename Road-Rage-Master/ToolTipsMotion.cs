0a457ed7fb5a5cdeacb91581a46d0b2a447daef4
Ting-Yu (Tiffany) Chang <33326947+tyc024@users.noreply.github.com>
2018-10-21 14:40:13 -0700

    Add files via upload

---
+++ Road-Rage-Master/ToolTipsMotion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolTipsMotion : MonoBehaviour {
+public GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (RemoveAfterSeconds (5, panel));
+	}
+	IEnumerator RemoveAfterSeconds(int seconds, GameObject obj){
+			yield return new WaitForSeconds(5);
+			obj.SetActive(false);
+		}
+
+	
+	// Update is called once per frame
+	void Update () {
+		//if player clicks ___ button, show tool tips panel
+		//if(Input.GetKey(KeyCode.JoyStick1Button14)){
+		//	panel.SetActive(true);
+		//	StartCoroutine(RemoveAfterSeconds(5, panel));
+		//}
+	}
+}
