89f2780a469828dda9148c9d59190544a6829d8f
Patrick J Pajarillaga <ppajaril@UCSD.EDU>
2018-11-11 15:25:30 -0800

    brake, speedometer, steering

--- Road-Rage-Master/Assets/speedometer/Speedometer.cs
+++ Road-Rage-Master/Assets/speedometer/Speedometer.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
 public class Speedometer : MonoBehaviour {
     private const float MIN_FILL_AMOUNT = 0.09802f;
     private const float MAX_FILL_AMOUNT = 0.90198f;
@@ -10,10 +12,11 @@ public class Speedometer : MonoBehaviour {
     private const float MIN_SPEED = 0.0f;
     private const float MAX_SPEED = 100.0f;
     private const float TOTAL_SPEED = MAX_SPEED - MIN_SPEED;
+    private float motorspeed;
 
     public Text speedText;
     public Image bar;
-
+    public Dot_Truck_Controller truck_controller;
     public float speed = 0;
 	
 	// Update is called once per frame
@@ -23,14 +26,15 @@ public class Speedometer : MonoBehaviour {
 	}
     
     void ChangeSpeed(float speed) {
-        this.speed = speed= LimitedSpeed(speed);
+        motorspeed = Mathf.Abs(truck_controller.motor1 /15);
+        this.speed = motorspeed;
         float fill_amount = MIN_FILL_AMOUNT + (speed / TOTAL_SPEED) * TOTAL_FILL_AMOUNT;
         bar.fillAmount = fill_amount;
         speedText.text = (int)speed+"";
     }
-    float LimitedSpeed(float speed) {
+    /*float LimitedSpeed(float speed) {
         if (speed > MAX_SPEED) return MAX_SPEED;
         if (speed < MIN_SPEED) return MIN_SPEED;
         return speed;
-    }
+    }*/
 }
