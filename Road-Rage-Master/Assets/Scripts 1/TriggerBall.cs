7bfc2bed8d58b245f11b26ea153184fbeaf1fdd8
Beverly Li <bel058@UCSD.EDU>
2018-12-02 16:16:01 -0800

    unity chan (and scale)

---
+++ Road-Rage-Master/Assets/Scripts 1/TriggerBall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBall : MonoBehaviour
+{
+    public Transform otherobj;
+    private GameObject[] buildings;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("oncollision");
+        if (collision.gameObject.CompareTag("Car"))
+        {
+            Debug.Log("collision is car");
+            //Vector3 vec = new Vector3(100F, 100F, 100F);
+            //otherobj.localScale = 1.01F*(otherobj.localScale);
+            buildings = GameObject.FindGameObjectsWithTag("Buildings");
+            foreach (GameObject b in buildings)
+                b.transform.localScale = 1.5F * (b.transform.localScale);
+        }
+    }
+}
