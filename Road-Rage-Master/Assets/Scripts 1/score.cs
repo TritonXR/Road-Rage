e46c08b5e72a1b458832e860a06b4ba9e7447102
Beverly Li <bel058@UCSD.EDU>
2019-03-03 17:57:54 -0800

    score obstacles

---
+++ Road-Rage-Master/Assets/Scripts 1/score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score : MonoBehaviour {
+    public GameObject carScript;
+    int times = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(times == 0)
+        {
+            carScript.GetComponent<Dot_Truck_Controller>().points++;
+            times++;
+        }
+        Debug.Log("POINTS: " + carScript.GetComponent<Dot_Truck_Controller>().points);
+        
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
